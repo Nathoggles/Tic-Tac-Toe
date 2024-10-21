daf4f0cc9c8dc126280a942a4512aea412ca262c
Nathoggles <mawricus@gmail.com>
2024-10-22 01:48:52 +0200

    set up initial makePlayer function

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 //we need a runGame, player and gameboard object
+//0, 1, 2 to each cell, or player=true?
+//win logic is on each move run rechwinner funct and that looks if x-1/y0 & x+1/y0 == 1[or true] || x0/y-1 & x0/y-1 == 1 || x+1/y+1 && x-1 && x-1 y-1 == 1 (but that only for center, check if need own logic for sides)
 
 
 function gameboard() {
@@ -15,5 +17,16 @@ function gameboard() {
     return board;
 }
 
-
+ 
 console.log(gameboard());
+
+function makePlayer(name){
+    let score = 0;
+    const getScore = () => score;
+    const upScore = () => score++;
+    return {name, getScore, upScore};
+}
+
+newPlayer = makePlayer("myname");
+console.log(newPlayer);
+
