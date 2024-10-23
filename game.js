3a928abe48eb7f2b441a609ae788c5c4513558c0
Nathoggles <mawricus@gmail.com>
2024-10-23 15:23:24 +0200

    add working move logic

--- game.js
+++ game.js
@@ -2,25 +2,6 @@
 //0, 1, 2 to each cell, or player=true?
 //win logic is on each move run rechwinner funct and that looks if x-1/y0 & x+1/y0 == 1[or true] || x0/y-1 & x0/y-1 == 1 || x+1/y+1 && x-1 && x-1 y-1 == 1 (but that only for center, check if need own logic for sides)
 
-
-function gameboard() {
-    const rows = 3;
-    const columns = 3;
-    const board = {};
-
-    for (let y = 1; y <= rows; y++) {
-        for (let x = 1; x <= columns; x++){
-            const coordinate = `x${x}y${y}`;
-            board[coordinate] = {x, y};
-        }
-    }
-    return board;
-}
-
- 
-/* console.log(gameboard()); */
-
-
 function MakePlayer(name){
     let score = 0;
     const getScore = () => score;
@@ -28,20 +9,51 @@ function MakePlayer(name){
     return {name, getScore, upScore};
 }
 
-Player1 = MakePlayer("Player1");
-Player2 = MakePlayer("Player2");
-/* console.log(Player1); */
 
-const board = gameboard();
+const gameboard = (function() {
+    const makeBoard = (x, y) => {
+    const rows = x;
+    const columns = y;
+    const board = {};
 
-console.log(board.x1y2.x);
+    for (let y = 1; y <= rows; y++) {
+        for (let x = 1; x <= columns; x++){
+            const coordinate = `x${x}y${y}`;
+            board[coordinate] = {x, y};
+            board[coordinate].state = 0;
+        }
+    }
+    return board;}
+    return {makeBoard};
+})();
 
-/* let testArray = ["x1y2", "x2y3"];
-console.log(testArray[1]); */
+/* console.log(gameboard.makeBoard(3, 3)); */
 
-//player chooses x2 y3
-//player chooses x3 
 
-/* function makeMove(x, y){
-    
-} */
+function playGame(){
+
+    player1 = MakePlayer("player1");
+    player2 = MakePlayer("player2");
+    let currentPlayer = player1;
+
+    const board = gameboard.makeBoard(3,3);
+
+
+    function makeMove(x, y, player){
+        board[`x${x}y${y}`].state = 1;
+        board[`x${x}y${y}`].player = player;
+        //console.log(board[`x${x}y${y}`]);
+        switchPlayer();
+    } 
+
+    function switchPlayer(){
+        currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1; 
+        console.log(currentPlayer);
+        }
+
+       makeMove(2, 3, currentPlayer);
+       makeMove(3, 3, currentPlayer);
+}
+
+playGame();
+
