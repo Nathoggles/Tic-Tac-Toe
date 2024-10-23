6ab52ca82f75c1e967b75b7c2ce08aba80de6af6
Nathoggles <mawricus@gmail.com>
2024-10-23 19:48:08 +0200

    start implementing different wincondition check

--- game.js
+++ game.js
@@ -32,28 +32,80 @@ const gameboard = (function() {
 
 function playGame(){
 
-    player1 = MakePlayer("player1");
-    player2 = MakePlayer("player2");
+    player1 = MakePlayer("Player1");
+    player2 = MakePlayer("Player2");
     let currentPlayer = player1;
 
     const board = gameboard.makeBoard(3,3);
 
 
     function makeMove(x, y, player){
-        board[`x${x}y${y}`].state = 1;
-        board[`x${x}y${y}`].player = player;
+        board[`x${x}y${y}`].state = player;
         //console.log(board[`x${x}y${y}`]);
+        checkWin(x, y, player);
         switchPlayer();
     } 
 
     function switchPlayer(){
         currentPlayer == player1 ? currentPlayer = player2 : currentPlayer = player1; 
-        console.log(currentPlayer);
+/*         console.log(currentPlayer); */
         }
 
-       makeMove(2, 3, currentPlayer);
-       makeMove(3, 3, currentPlayer);
-}
+/*        makeMove(1, 2, currentPlayer);
+         makeMove(3, 3, currentPlayer);
+       makeMove(3, 2, currentPlayer);
+         makeMove(3, 1, currentPlayer); */
+       makeMove(2, 2, currentPlayer);
+  
+
+
+       function checkWin(x, y, player){
+
+        function isX(coord){
+            return coord.x == x;
+        }
+    const filteredX = Object.keys(board.map)//.filter(isX);
+    console.log(filteredX);
+        
+
+  /*       const directions = [
+            {dx: 1, dy: 0}, //right
+            {dx: 0, dy: 1}, //down
+            {dx: 1, dy: 1}, //down-right
+            {dx: 1, dy: -1} //down-left
+        ]
+        console.log(board);
+        Object.keys(board).forEach(key => {
+            if (board[key].state == player) {
+                if (board[key])
+            }
+        }
+            
+            console.log(board[key].x, board[key].y)); */
+/*     for ({dx, dy} of directions){
+
+    }  */
+    
+      /*   function countCells(dx, dy)
+        {
+            let count = 0;
+            let nx = x;
+            let ny = y;
+            while (board)
+        } */
+
+       }
+       
+  /*       let currentCell = [`x${x}y${y}`];
+        console.log(currentCell); */
+        /* if (board[`x${x + 1}y${y}`].state == player && board[`x${x - 1}y${y}`].state == player ||
+            board[`x${x - 1}y${y - 1}`].state == player && board[`x${x - 2 }y${y - 2}`].state == player
+            //pontentially a lot of win conditions to check
+
+        ) */
+       
+            //alert(player.name + " has won")}; 
+
+    }
 
 playGame();
-
