25f52882979de4bcaec5fcab20918bd798e5fa2a
Nathoggles <mawricus@gmail.com>
2024-10-23 20:13:34 +0200

    implement player move tracking

--- game.js
+++ game.js
@@ -2,12 +2,6 @@
 //0, 1, 2 to each cell, or player=true?
 //win logic is on each move run rechwinner funct and that looks if x-1/y0 & x+1/y0 == 1[or true] || x0/y-1 & x0/y-1 == 1 || x+1/y+1 && x-1 && x-1 y-1 == 1 (but that only for center, check if need own logic for sides)
 
-function MakePlayer(name){
-    let score = 0;
-    const getScore = () => score;
-    const upScore = () => score++;
-    return {name, getScore, upScore};
-}
 
 
 const gameboard = (function() {
@@ -31,16 +25,28 @@ const gameboard = (function() {
 
 
 function playGame(){
-
+    const board = gameboard.makeBoard(3,3);
+    
+    function MakePlayer(name){
+        let score = 0;
+        const getScore = () => score;
+        const upScore = () => score++;
+        board[name] = []; //array to track each player's moves
+        return {name, getScore, upScore};
+    }
     player1 = MakePlayer("Player1");
     player2 = MakePlayer("Player2");
     let currentPlayer = player1;
 
-    const board = gameboard.makeBoard(3,3);
+
+
+   
 
 
     function makeMove(x, y, player){
-        board[`x${x}y${y}`].state = player;
+    
+        board[`x${x}y${y}`].state = 1;
+        board[player.name].push({x, y});
         //console.log(board[`x${x}y${y}`]);
         checkWin(x, y, player);
         switchPlayer();
@@ -60,12 +66,12 @@ function playGame(){
 
 
        function checkWin(x, y, player){
-
-        function isX(coord){
-            return coord.x == x;
-        }
-    const filteredX = Object.keys(board.map)//.filter(isX);
-    console.log(filteredX);
+        console.log(element);
+     /*    player.name.forEach(element => {
+           
+        }); */
+    //const filteredX = Object.keys(board.map)//.filter(isX);
+    //console.log(filteredX);
         
 
   /*       const directions = [
